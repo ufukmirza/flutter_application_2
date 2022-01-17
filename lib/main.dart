927e8ee6d79e893cec8df4abd022f6b86442dbf8
YigitKaanBingol <yigit.kaan67@gmail.com>
2022-01-17 17:34:32 +0300

    merraba

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-            child: Text('Mirza Second Commit'),
+            child: Text('Mirza Second Cojjjkmmit'),
           ),
         ),
       ),
